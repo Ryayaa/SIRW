afdbe426357712bc3ed6f085b7e77ad3c3d5c941
Andreagazy <andreagazyia@gmail.com>
2024-05-03 01:11:29 +0700

    Landig & Login

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/index', function () {
+    return view('index');
+});
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/login', function () {
+    return view('login');
+});
+
