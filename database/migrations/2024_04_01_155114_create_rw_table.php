c00a74f3fdcdb2312bf19be790d34c8a7c2e17d5
Andreagazy <andreagazyia@gmail.com>
2024-05-02 17:56:12 +0700

    Migration Database

---
+++ database/migrations/2024_04_01_155114_create_rw_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('rw', function (Blueprint $table) {
+            $table->id('id_rw');
+            $table->string('username',45);
+            $table->string('nama_lengkap', 100);
+            $table->enum('jenis_kelamin', ['L', 'P']);
+            $table->string('alamat', 255);
+            $table->string('no_telepon',45);
+            $table->string('password');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('rw');
+    }
+};
