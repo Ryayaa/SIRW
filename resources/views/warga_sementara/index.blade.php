b1dfd59afafd8c1d499a0af25f35cc4c999a2844
Ridho Fauzian Pratama <ridhofp3@gmail.com>
2024-05-28 10:07:42 +0700

    update index

--- resources/views/warga_sementara/index.blade.php
+++ resources/views/warga_sementara/index.blade.php
@@ -49,16 +49,6 @@
                 { data: 'id_warga_sementara', name: 'id_warga_sementara' },
                 { data: 'nik', name: 'nik' },
                 { data: 'nama_lengkap', name: 'nama_lengkap' },
-                { data: 'tanggal_lahir', name: 'tanggal_lahir' },
-                { data: 'jenis_kelamin', name: 'jenis_kelamin' },
-                { data: 'alamat_asal', name: 'alamat_asal' },
-                { data: 'alamat_domisili', name: 'alamat_domisili' },
-                { data: 'pekerjaan', name: 'pekerjaan' },
-                { data: 'status_perkawinan', name: 'status_perkawinan' },
-                { data: 'tanggal_masuk', name: 'tanggal_masuk' },
-                { data: 'bukti_ktp', name: 'bukti_ktp', render: function(data, type, full, meta) {
-                    return data ? `<img src="` + baseUrl + data + `" width="50" height="50" alt="Bukti KTP">` : 'No Image';
-                }},
                 { data: 'aksi', name: 'aksi', orderable: false, searchable: false },
             ]
         });
