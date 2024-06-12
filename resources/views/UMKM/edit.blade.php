48183d73415b9b4e5dca404527404eb3f4aca47d
Arrya Fitriansyah <= arrya306@gmail.com>
2024-06-12 11:07:32 +0700

    PERBAIKI TAMPILAN UMKM

--- resources/views/UMKM/edit.blade.php
+++ resources/views/UMKM/edit.blade.php
@@ -22,8 +22,8 @@
                 {!! method_field('PUT') !!}
 
                 <div class="form-group row">
-                    <label class="col-1 control-label col-form-label">Nama UMKM</label>
-                    <div class="col-11">
+                    <label class="col-md-2 col-form-label">Nama UMKM</label>
+                    <div class="col-md-10">
                         <input type="text" class="form-control" id="nama_umkm" name="nama_umkm"
                             value="{{ old('nama_umkm', $umkm->nama_umkm) }}" required>
                         @error('nama_umkm')
@@ -32,8 +32,8 @@
                     </div>
                 </div>
                 <div class="form-group row">
-                    <label class="col-1 control-label col-form-label">Alamat</label>
-                    <div class="col-11">
+                    <label class="col-md-2 col-form-label">Alamat</label>
+                    <div class="col-md-10">
                         <input type="text" class="form-control" id="alamat" name="alamat"
                             value="{{ old('alamat', $umkm->alamat) }}" required>
                         @error('alamat')
@@ -42,8 +42,8 @@
                     </div>
                 </div>
                 <div class="form-group row">
-                    <label class="col-1 control-label col-form-label">Deskripsi</label>
-                    <div class="col-11">
+                    <label class="col-md-2 col-form-label">Deskripsi</label>
+                    <div class="col-md-10">
                         <textarea class="form-control" id="deskripsi" name="deskripsi" rows="3" required>{{ old('deskripsi', $umkm->deskripsi) }}</textarea>
                         @error('deskripsi')
                             <small class="form-text text-danger">{{ $message }}</small>
@@ -51,8 +51,8 @@
                     </div>
                 </div>
                 <div class="form-group row">
-                    <label class="col-1 control-label col-form-label">No Telepon</label>
-                    <div class="col-11">
+                    <label class="col-md-2 col-form-label">No Telepon</label>
+                    <div class="col-md-10">
                         <input type="text" class="form-control" id="no_telepon" name="no_telepon"
                             value="{{ old('no_telepon', $umkm->no_telepon) }}" required>
                         @error('no_telepon')
@@ -61,8 +61,8 @@
                     </div>
                 </div>
                 <div class="form-group row">
-                    <label class="col-1 control-label col-form-label">Gambar</label>
-                    <div class="col-11">
+                    <label class="col-md-2 col-form-label">Gambar</label>
+                    <div class="col-md-10">
                         <input type="file" class="form-control-file" id="gambar" name="gambar">
                         @if ($umkm->gambar)
                             <img src="{{ asset('images/' . $umkm->gambar) }}" alt="{{ $umkm->nama_umkm }}" class="img-thumbnail mt-2" style="max-height: 200px;">
@@ -75,8 +75,8 @@
                     </div>
                 </div>
                 <div class="form-group row">
-                    <label class="col-1 control-label col-form-label">ID Warga</label>
-                    <div class="col-11">
+                    <label class="col-md-2 col-form-label">ID Warga</label>
+                    <div class="col-md-10">
                         <input type="text" class="form-control" id="id_warga" name="id_warga"
                             value="{{ old('id_warga', $umkm->id_warga) }}" required>
                         @error('id_warga')
@@ -85,8 +85,8 @@
                     </div>
                 </div>
                 <div class="form-group row">
-                    <label class="col-1 control-label col-form-label">Status Pengajuan</label>
-                    <div class="col-11">
+                    <label class="col-md-2 col-form-label">Status Pengajuan</label>
+                    <div class="col-md-10">
                         <select name="status_pengajuan" class="form-control">
                             <option value="1" {{ old('status_pengajuan', $umkm->status_pengajuan) == 1 ? 'selected' : '' }}>Disetujui</option>
                             <option value="0" {{ old('status_pengajuan', $umkm->status_pengajuan) == 0 ? 'selected' : '' }}>Ditolak</option>
@@ -95,8 +95,7 @@
                     </div>
                 </div>
                 <div class="form-group row">
-                    <label class="col-1 control-label col-form-label"></label>
-                    <div class="col-11">
+                    <div class="col-md-10 offset-md-2">
                         <button type="submit" class="btn btn-primary btn-sm">Simpan</button>
                         <a class="btn btn-sm btn-default ml-1" href="{{ url('umkm') }}">Kembali</a>
                     </div>
